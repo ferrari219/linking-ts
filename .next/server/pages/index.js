8920c99818e4abc9ed1aa29950d26b418bc8f094
ferrari219@nate.com <ferrari219@nate.com>
2021-11-18 15:30:35 +0900

    emotion 설치 및 ReadMe 수정

---
+++ .next/server/pages/index.js
@@ -0,0 +1,46 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(() => {
+var exports = {};
+exports.id = "pages/index";
+exports.ids = ["pages/index"];
+exports.modules = {
+
+/***/ "./pages/index.tsx":
+/*!*************************!*\
+  !*** ./pages/index.tsx ***!
+  \*************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-runtime */ \"react/jsx-runtime\");\n/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__);\n\nconst index = ()=>{\n    return(/*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)(\"div\", {\n        __source: {\n            fileName: \"D:\\\\developer\\\\test\\\\linking-ts\\\\pages\\\\index.tsx\",\n            lineNumber: 2,\n            columnNumber: 10\n        },\n        __self: undefined,\n        children: \"test\"\n    }));\n};\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (index);\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9wYWdlcy9pbmRleC50c3guanMiLCJtYXBwaW5ncyI6Ijs7Ozs7OztBQUFBLEtBQUssQ0FBQ0EsS0FBSyxPQUFTLENBQUM7SUFDbkIsTUFBTSxzRUFBRUMsQ0FBRzs7Ozs7OztrQkFBQyxDQUFJOztBQUNsQixDQUFDO0FBRUQsaUVBQWVELEtBQUssRUFBQyIsInNvdXJjZXMiOlsid2VicGFjazovL2xpbmtpbmcvLi9wYWdlcy9pbmRleC50c3g/MDdmZiJdLCJzb3VyY2VzQ29udGVudCI6WyJjb25zdCBpbmRleCA9ICgpID0+IHtcclxuICByZXR1cm4gPGRpdj50ZXN0PC9kaXY+O1xyXG59O1xyXG5cclxuZXhwb3J0IGRlZmF1bHQgaW5kZXg7XHJcbiJdLCJuYW1lcyI6WyJpbmRleCIsImRpdiJdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///./pages/index.tsx\n");
+
+/***/ }),
+
+/***/ "react/jsx-runtime":
+/*!************************************!*\
+  !*** external "react/jsx-runtime" ***!
+  \************************************/
+/***/ ((module) => {
+
+module.exports = require("react/jsx-runtime");
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = (__webpack_exec__("./pages/index.tsx"));
+module.exports = __webpack_exports__;
+
+})();
--- README.md
+++ README.md
@@ -1,34 +1,62 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+# Livart Linking
 
-## Getting Started
+'리바트 링킹' 이란?
 
-First, run the development server:
+리바트 매장 내 잔여 공간, 시간을 활용해 원데이 클래스&플리마켓 운영 신규사업제안.
+사내 공모전 최우수상 수상
 
-```bash
-npm run dev
-# or
-yarn dev
-```
+### 미리보기
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+[구축 보기](https://ferrari219.github.io/linking)
 
-You can start editing the page by modifying `pages/index.tsx`. The page auto-updates as you edit the file.
+[프로토타입 보기](https://xd.adobe.com/view/396a5d24-354e-468e-45e6-cb3d07819689-4f5b/screen/6d629aa4-a2c2-4650-868a-3c91d0f62c51)
 
-[API routes](https://nextjs.org/docs/api-routes/introduction) can be accessed on [http://localhost:3000/api/hello](http://localhost:3000/api/hello). This endpoint can be edited in `pages/api/hello.ts`.
+![2021-03-17 12 03 03](https://user-images.githubusercontent.com/16126002/111408552-cb9d9880-8718-11eb-90b1-2c70d7958b8b.png)
 
-The `pages/api` directory is mapped to `/api/*`. Files in this directory are treated as [API routes](https://nextjs.org/docs/api-routes/introduction) instead of React pages.
+디자인 업그레이드 과정
 
-## Learn More
+![linking_thumb1](https://user-images.githubusercontent.com/16126002/111254431-6bdfb880-8658-11eb-9de3-f46c5437fa4b.jpg)
 
-To learn more about Next.js, take a look at the following resources:
+## Dev Install Module
 
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
+- [x] React.js
+- [x] next.js
+- [x] typescript
+- [x] axios
+- [x] storybook
+- [x] emotion
+- [x] svgr/webpack
 
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
+## Design Tool
 
-## Deploy on Vercel
+- [x] Adobe XD
 
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
+## Pages
 
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+- [x] Home: 메인화면
+- [x] Detail: 클래스 상세화면
+- [x] Calendar: 달력 일정 보기
+
+## Components
+
+[스토리북 확인하기]()
+
+Atomic Design 방식
+
+- [x] 01_Atom: 단일기능(버튼, 인풋, 아이콘, li 등)
+- [x] 02_Molecules: 2가지 이상 복합 컴포넌트 / Wrap(ul)
+- [x] 03_Organism: Atom+Molecules
+- [x] 04_Templates: 기능 제어 가능한 모듈 단위
+- [x] 99_Etc: Heads, Layout
+
+## 개발 이야기
+
+지금껏 거론 된적 없는 아이디어 조각들을 모아 하나의 서비스로 만드는 과정을
+직접 경험해보는것도 색다른 경험이었습니다.
+기획부터 디자인, 프론트엔드까지 모두 할수 있었거든요.
+
+디자인은 다소 급조된 부분이 있어 아쉽게 생각됩니다.
+차후 Figma로 다시 만들어보면서 디자인 다듬어 볼 생각입니다.
+
+프론트엔드는 리액트를 사용했습니다.
+스타일은 SASS를 사용했고, 절대경로 처리를 위해 craco를 썼습니다.
